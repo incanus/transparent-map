636daaa9f142d8518b75da7a0760928a38f91ef7
Justin R. Miller <incanus@codesorcery.net>
2013-08-26 11:28:46 -0700

    Initial Commit

---
+++ Clear Map/MBXViewController.h
@@ -0,0 +1,13 @@
+//
+//  MBXViewController.h
+//  Clear Map
+//
+//  Created by Justin R. Miller on 8/26/13.
+//  Copyright (c) 2013 MapBox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBXViewController : UIViewController
+
+@end
---
+++ Clear MapTests/Clear_MapTests.m
@@ -0,0 +1,34 @@
+//
+//  Clear_MapTests.m
+//  Clear MapTests
+//
+//  Created by Justin R. Miller on 8/26/13.
+//  Copyright (c) 2013 MapBox. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Clear_MapTests : XCTestCase
+
+@end
+
+@implementation Clear_MapTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
