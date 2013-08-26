01147a854829cf0fa34eb690f00c9f4ed3516e46
Justin R. Miller <incanus@codesorcery.net>
2013-08-26 11:46:57 -0700

    basic map view

--- Clear Map/MBXViewController.m
+++ Clear Map/MBXViewController.m
@@ -8,11 +8,17 @@
 
 #import "MBXViewController.h"
 
+#import <MapBox/MapBox.h>
+
 @implementation MBXViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
+    RMMapView *mapView = [[RMMapView alloc] initWithFrame:CGRectMake(100, 100, self.view.bounds.size.width - 200, self.view.bounds.size.height - 200)];
+    mapView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [self.view addSubview:mapView];
 }
 
 @end
